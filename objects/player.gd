284b11c434d183b9b86503d0bbb8add8a2af0989
Martin Jakubik <martindzejky@gmail.com>
2025-03-22 15:47:19 +0100

    jumping

--- objects/player.gd
+++ objects/player.gd
@@ -3,35 +3,41 @@ extends CharacterBody2D
 @export var current_speed = 800
 @export var gravity = 1000.0
 @export var slope_snap_distance = 1.0  # Distance to snap to slope
+@export var jump_strength = 500.0
 
 @export var sprite: Sprite2D
 
 var slope_normal = Vector2.UP
 
 func _physics_process(delta):
-  if is_on_floor():
-    # Movement aligned with slope
-    var slope_direction = slope_normal.rotated(PI/2)  # 90 degrees from normal
-    velocity = slope_direction * current_speed
+  if Input.is_action_just_pressed("jump") and is_on_floor():
+    handle_jump()
+  elif is_on_floor():
+    handle_slope_movement()
   else:
     handle_air_movement(delta)
 
   move_and_slide()
 
-  # Try to snap to slopes when in air and moving downward
   if !is_on_floor():
     snap_to_slope()
 
   if is_on_floor():
-    # Store the new slope normal if we're on ground
     slope_normal = get_floor_normal()
+    update_sprite_rotation()
 
-    # Rotate the sprite to match the slope
-    sprite.rotation = slope_normal.angle() + PI / 2
-
+    # TODO: just for debug purposes
     sprite.modulate = Color.RED
   else:
     sprite.modulate = Color.GREEN
+
+func handle_slope_movement():
+  # Movement aligned with slope
+  var slope_direction = slope_normal.rotated(PI/2)  # 90 degrees from normal
+  velocity = slope_direction * current_speed
+
+func handle_jump():
+  velocity.y = -jump_strength
 
 func handle_air_movement(delta):
   # Apply gravity to change direction
@@ -39,6 +45,10 @@ func handle_air_movement(delta):
 
   # Normalize to maintain constant speed
   velocity = velocity.normalized() * current_speed
+
+func update_sprite_rotation():
+  # Rotate the sprite to match the slope
+  sprite.rotation = slope_normal.angle() + PI / 2
 
 func snap_to_slope():
   # Test movement downward
