b3ddd66b043ffacd067f371873b16fa604ff3224
Martin Jakubik <martindzejky@gmail.com>
2025-03-22 19:36:02 +0100

    delete print lefovers

--- sprites/delete_when_far_away.gd
+++ sprites/delete_when_far_away.gd
@@ -8,6 +8,4 @@ func _process(_delta):
   var player = get_tree().get_first_node_in_group("player")
   if player:
     if what_to_delete.global_position.x < player.global_position.x - distance_to_delete:
-      print("Deleting ", what_to_delete.name)
-      print("Distance: ", what_to_delete.global_position.x - player.global_position.x)
       what_to_delete.queue_free()
