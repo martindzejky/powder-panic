4fb4124e306c4b20b1e4dd20176a35ce45649494
Martin Jakubik <martindzejky@gmail.com>
2025-03-22 14:10:53 +0100

    switch chunks to curves

---
+++ objects/chunk.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var path: Path2D
+@export var line: Line2D
+@export var shape: CollisionPolygon2D
+
+const BOTTOM_OFFSET = 1000
+
+func _ready():
+  # Initialize the line and the shape collider based on the path curve
+  var points := path.curve.tessellate(4) # TODO: play with this value
+  var polygon_points = points.duplicate()
+
+  line.points = points
+
+  # Add 2 bottom points to the shape collider to make it a closed polygon.
+  # Let's make it BOTTOM_OFFSET units below the last point.
+  polygon_points.append(points[-1] + Vector2.DOWN * BOTTOM_OFFSET)
+  polygon_points.append(points[0] + Vector2.DOWN * BOTTOM_OFFSET)
+
+  shape.polygon = polygon_points
