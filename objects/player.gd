da997c833a5c19fca143e017025e2a62a191fba4
Martin Jakubik <martindzejky@gmail.com>
2025-03-22 15:52:29 +0100

    delete snapping for now

--- objects/player.gd
+++ objects/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var current_speed = 800
 @export var gravity = 1000.0
-@export var slope_snap_distance = 1.0  # Distance to snap to slope
 @export var jump_strength = 500.0
 
 @export var sprite: Sprite2D
@@ -18,9 +17,6 @@ func _physics_process(delta):
     handle_air_movement(delta)
 
   move_and_slide()
-
-  if !is_on_floor():
-    snap_to_slope()
 
   if is_on_floor():
     slope_normal = get_floor_normal()
@@ -49,19 +45,3 @@ func handle_air_movement(delta):
 func update_sprite_rotation():
   # Rotate the sprite to match the slope
   sprite.rotation = slope_normal.angle() + PI / 2
-
-func snap_to_slope():
-  # Test movement downward
-  var test_motion = Vector2(0, slope_snap_distance)
-  var collision = move_and_collide(test_motion, true) # true = test only
-
-  if collision:
-    # We found a slope!
-    slope_normal = collision.get_normal()
-
-    # Move to the snap position
-    velocity = test_motion
-    move_and_slide()
-
-    # Restore the proper velocity for next frame
-    velocity = slope_normal.rotated(PI/2) * current_speed
