fd0e459b4e10ce52fceb5c32a8e47a0b2924c88e
Martin Jakubik <martindzejky@gmail.com>
2025-03-29 23:01:47 +0100

    simplify chunk debug

--- chunks/chunk_debug.gd
+++ chunks/chunk_debug.gd
@@ -7,11 +7,6 @@ class_name ChunkDebug
     chunk = value
     update_configuration_warnings()
 
-@export var path: Path2D:
-  set(value):
-    path = value
-    update_configuration_warnings()
-
 @onready var default_font = ThemeDB.fallback_font
 
 func _get_configuration_warnings() -> PackedStringArray:
@@ -20,16 +15,13 @@ func _get_configuration_warnings() -> PackedStringArray:
   if chunk == null:
     warnings.append("Chunk is not set")
 
-  if path == null:
-    warnings.append("Path is not set")
-
   return warnings
 
 func _process(_delta) -> void:
   if not Engine.is_editor_hint():
     return
 
-  if chunk == null or path == null:
+  if chunk == null:
     return
 
   queue_redraw()
@@ -38,33 +30,24 @@ func _draw() -> void:
   if not Engine.is_editor_hint():
     return
 
-  if chunk == null or path == null:
+  if chunk == null:
     return
 
   # Get the start and end points of the chunk
-  var start_point = path.curve.get_point_position(0)
-  var start_dir = path.curve.get_point_out(0)
-  var end_point = path.curve.get_point_position(path.curve.get_point_count() - 1)
-  var end_dir = path.curve.get_point_in(path.curve.get_point_count() - 1)
+  var start_point = chunk.get_start_point()
+  var start_angle = chunk.get_start_angle()
+  var start_angle_rad = deg_to_rad(start_angle)
+  var end_point = chunk.get_end_point()
+  var end_angle = chunk.get_end_angle()
+  var end_angle_rad = deg_to_rad(end_angle)
 
-  # Draw an arrows
-  draw_line(start_point, start_point - start_dir, Color.RED, 2)
-  draw_line(end_point, end_point - end_dir, Color.RED, 2)
-
-  # Display the angles
-  var start_angle_rounded = calculate_start_angle(start_dir)
-  var end_angle_rounded = calculate_end_angle(end_dir)
+  var arrow_length = 40
   var offset = Vector2(0, -16)
 
-  draw_string(default_font, start_point + offset, str(start_angle_rounded) + '°', HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.RED)
-  draw_string(default_font, end_point + offset, str(end_angle_rounded) + '°', HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.RED)
+  # Draw an arrows
+  draw_line(start_point, start_point + Vector2.LEFT.rotated(start_angle_rad) * arrow_length, Color.RED, 2)
+  draw_line(end_point, end_point + Vector2.RIGHT.rotated(end_angle_rad) * arrow_length, Color.RED, 2)
 
-func calculate_start_angle(direction: Vector2) -> float:
-  var angle = rad_to_deg(direction.angle())
-  var angle_180 = fmod(angle + 180.0, 360.0) - 180.0
-  return roundf(angle_180 * 100) / 100
-
-func calculate_end_angle(direction: Vector2) -> float:
-  var angle = rad_to_deg(direction.angle()) + 180.0
-  var angle_180 = fmod(angle + 180.0, 360.0) - 180.0
-  return roundf(angle_180 * 100) / 100
+  # Display the angles
+  draw_string(default_font, start_point + offset, str(start_angle) + '°', HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.RED)
+  draw_string(default_font, end_point + offset, str(end_angle) + '°', HORIZONTAL_ALIGNMENT_CENTER, -1, 16, Color.RED)
