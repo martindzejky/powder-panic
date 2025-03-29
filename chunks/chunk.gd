3bd41d98f25163d4cd14f21bc84db721f222e3ae
Martin Jakubik <martindzejky@gmail.com>
2025-03-29 23:42:46 +0100

    implement the terrain gradient

--- chunks/chunk.gd
+++ chunks/chunk.gd
@@ -3,34 +3,58 @@ extends Node2D
 class_name Chunk
 
 @export var path: Path2D
-@export var line: Line2D
 @export var polygon: Polygon2D
 @export var shape: CollisionPolygon2D
 
 @export_tool_button('Snap angles') var snap_angles_button := snap_angles
+@export_tool_button('Update shape') var update_shape_button := update_shape
 
 const BOTTOM_OFFSET = 500
 
 func _ready():
-  update_shape()
-
-func update_shape():
   # Do NOT run in the editor
   if Engine.is_editor_hint():
     return
 
+  update_shape()
+
+func update_shape():
   # Initialize the line and the shape collider based on the path curve
   var points := path.curve.tessellate(4, 2) # TODO: play with this value
-  var polygon_points = points.duplicate()
+  var polygon_points := points.duplicate()
 
-  # Add 2 bottom points to the shape collider to make it a closed polygon.
-  # Let's make it BOTTOM_OFFSET units below the last point.
-  polygon_points.append(points[-1] + Vector2.DOWN * BOTTOM_OFFSET)
-  polygon_points.append(points[0] + Vector2.DOWN * BOTTOM_OFFSET)
+  # Copy the line to the bottom of the polygon but in reverse order,
+  # so we essentially get a super thick "line".
+  var points_reversed := points.duplicate()
+  points_reversed.reverse()
 
-  line.points = points
+  for point in points_reversed:
+    polygon_points.append(point + Vector2.DOWN * BOTTOM_OFFSET)
+
+  # Assign the points to the line and the polygon
   polygon.polygon = polygon_points
   shape.polygon = polygon_points
+
+  # Now generate the UV coordinates for the polygon.
+  # Each point on the top line should be between 0,0 and 1,0 (left to right).
+  # The points on the bottom line should be between 0,1 and 1,1 (left to right).
+  var uv_points: PackedVector2Array = []
+  var x_min = points[0].x
+  var x_max = points[-1].x
+  var texture_width = polygon.texture.get_width()
+  var texture_height = polygon.texture.get_height()
+
+  # UVs for the top line
+  for point in points:
+    uv_points.append(Vector2((point.x - x_min) / (x_max - x_min) * texture_width, 0))
+    print(uv_points[-1])
+
+  # UVs for the bottom line
+  for point in points_reversed:
+    uv_points.append(Vector2((point.x - x_min) / (x_max - x_min) * texture_width, texture_height))
+    print(uv_points[-1])
+
+  polygon.uv = uv_points
 
 func get_start_point():
   return path.curve.get_point_position(0)
